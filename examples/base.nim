0c3962a93c7068bc01210954eb5047cc91d77388
Rostyslav Ivasiv <rivasiv@gmail.com>
2016-11-18 09:21:42 +0200

    Update base.nim
    Small clean up.

--- examples/base.nim
+++ examples/base.nim
@@ -42,26 +42,3 @@ suite "Basic tests." :
     status = fdb_shutdown()
     check(status == FDB_RESULT_SUCCESS)
 
-
-#// Open a database
-#db, _ := Open("test", nil)
-
-#// Close it properly when we're done
-#defer db.Close()
-
-#// Store the document
-#doc, _ := NewDoc([]byte("key"), nil, []byte("value"))
-#defer doc.Close()
-#db.Set(doc)
-
-#// Lookup the document
-#doc2, _ := NewDoc([]byte("key"), nil, nil)
-#defer doc2.Close()
-#db.Get(doc2)
-
-#// Delete the document
-#doc3, _ := NewDoc([]byte("key"), nil, nil)
-#defer doc3.Close()
-#db.Delete(doc3)
-
-
